f3a2eb39bf8e60207a7c8d8cc0489ef4a393a847
hyprox1413 <hyprox1413@gmail.com>
2026-05-16 19:59:36 -0400

    allow all cors

--- backend/index.ts
+++ backend/index.ts
@@ -11,7 +11,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server({
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
   },
 });
 
@@ -24,7 +24,7 @@ const roomsByPlayer = new Map<string, RoomInfo>();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
   }),
 );
 
