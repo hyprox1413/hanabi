8d722852a75d1ac9cca4e4b63d8d7bc818d1f4c4
hyprox1413 <hyprox1413@gmail.com>
2026-05-16 21:32:08 -0400

    change to url envvar

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-export const HTTP_URL = `http://${import.meta.env.VITE_HOSTNAME ?? "localhost"}:3000`;
-export const SOCK_URL = `http://${import.meta.env.VITE_HOSTNAME ?? "localhost"}:4000`;
+export const HTTP_URL = import.meta.env.VITE_HTTP_URL ?? "http://localhost:3000";
+export const SOCK_URL = import.meta.env.VITE_SOCK_URL ?? "http://localhost:4000";
 
 export const socket = io(SOCK_URL);
