50794eee4c3670191c74dc48a6aaca8837b4d217
hyprox1413 <hyprox1413@gmail.com>
2026-05-16 20:16:13 -0400

    get hostname from envvar

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-export const HTTP_URL = "http://localhost:3000";
-export const SOCK_URL = "http://localhost:4000";
+export const HTTP_URL = `http://${import.meta.env.HOSTNAME ?? "localhost"}:3000`;
+export const SOCK_URL = `http://${import.meta.env.HOSTNAME ?? "localhost"}:4000`;
 
 export const socket = io(SOCK_URL);
