f4d1bb60342c5a1f79d15450b325cc65c71954ea
hyprox1413 <hyprox1413@gmail.com>
2026-05-23 23:59:15 -0400

    played cards which lose a life now add to discard pile

--- util/game.ts
+++ util/game.ts
@@ -56,6 +56,7 @@ export function makeMove(game: GameState, move: MoveInfo): boolean {
           }
         }
       } else {
+        game.discarded.push(card);
         game.livesRemaining--;
       }
     }
